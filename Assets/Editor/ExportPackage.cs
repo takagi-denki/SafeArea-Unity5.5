8e857f8d7819e6faac1a5598ccdac94a2244328c
mtakagi <mtakagi127@live.jp>
2018-06-13 01:11:31 +0900

    Add package export script

---
+++ Assets/Editor/ExportPackage.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public static class ExportPackage {
+
+	[MenuItem("Assets/Export SafeArea Package")]
+	public static void Export() {
+		var names = new []{"Assets/Scripts/ScreenUtil.cs", 
+						   "Assets/Scripts/Plugins/iOS/SafeArea.cs",
+						   "Assets/Scripts/Plugins/iOS/SafeArea.mm"};
+		if (!Directory.Exists (Path.Combine(Directory.GetCurrentDirectory(), "build"))) {
+			Directory.CreateDirectory (Path.Combine(Directory.GetCurrentDirectory(), "build"));
+		}
+
+		AssetDatabase.ExportPackage (names, "build/SafeArea.unitypackage");
+	}
+
+}
